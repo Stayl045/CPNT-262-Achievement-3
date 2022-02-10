e4f3e6760f9d19faccca023e5d602a2e722b790f
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-10 08:28:14 -0700

    a commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const slider = document.querySelector(".slider");
 const before = document.querySelector(".img-container-before");
 const after = document.querySelector(".img-container-after");
 const container = document.querySelector(".container");
+
 const sliding = (e) => {
   let xPos = e.layerX;
   before.style.width = xPos + "px";
