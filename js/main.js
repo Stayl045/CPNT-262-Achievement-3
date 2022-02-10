03ee554031ce458cdc68617ed095a7ba7fe4c364
Stephan Taylor <stephantaylor21@hotmail.com>
2022-02-10 08:25:19 -0700

    im not sure

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const slider = document.querySelector(".slider");
 const before = document.querySelector(".img-container-before");
 const after = document.querySelector(".img-container-after");
 const container = document.querySelector(".container");
-
 const sliding = (e) => {
   let xPos = e.layerX;
   before.style.width = xPos + "px";
